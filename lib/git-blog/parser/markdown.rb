f21a1825703c15cb46b9c763ccc31f15983385db
elliottcable <me@elliottcable.name>
2008-05-18 02:22:51 -0800

    Maruku screwed up, switched back

--- lib/git-blog/parser/markdown.rb
+++ lib/git-blog/parser/markdown.rb
@@ -1,11 +1,11 @@
-require 'maruku'
+require 'redcloth'
 
 module GitBlog
   module Parsers
     module Markdown
       def self.parse input
         input.gsub!(/^(.*)\n=+(\n\s+)*\n/m, '')
-        ::Maruku.new(input).to_html
+        ::RedCloth.new(input).to_html
       end
     end
   end
