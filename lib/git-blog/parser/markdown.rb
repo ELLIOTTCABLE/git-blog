7f07e67dc890210f119176222c447b43ed32c60b
elliottcable <git@elliottcable.name>
2008-06-19 16:19:30 -0800

    Now supporting 4 Markdown parsers

--- lib/git-blog/parser/markdown.rb
+++ lib/git-blog/parser/markdown.rb
@@ -1,13 +1,34 @@
-require 'maruku'
+# rpeg-markdown
+begin
+  require 'markdown'
+  
+# Discount
+rescue LoadError
+  begin
+    require 'rdiscount'
+    Markdown = RDiscount
+  
+  # Maruku
+  rescue LoadError
+    begin
+      require 'maruku'
+      Markdown = Maruku
+    
+    # BlueCloth
+    rescue LoadError
+      require 'bluecloth'
+      Markdown = BlueCloth
+    end
+  end
+end
 
 module GitBlog
   module Parsers
     module Markdown
       def self.parse input
         input.gsub!(/^(.*)\n=+(\n\s+)*\n/m, '')
-        ::Maruku.new(input).to_html
+        ::Markdown.new(input).to_html
       end
     end
   end
 end
-
