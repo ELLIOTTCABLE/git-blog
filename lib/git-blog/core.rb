97ad7c68902d136da034c539fe5d43d68ad38285
elliottcable <git@elliottcable.name>
2008-06-26 00:17:38 -0800

    Screw that, using a constant.

--- lib/git-blog/core.rb
+++ lib/git-blog/core.rb
@@ -12,9 +12,11 @@ module GitBlog
   
   # Returns the path of file relative to the git-blog root.
   # 'Borrowed' mostly wholesale from Haml 2.0.0 d-:
-  def self.Scope path
-    File.join(File.expand_path( File.dirname(__FILE__) / '..' / '..' ), path.to_s)
-  end
+  # def self.Scope path
+  #   File.join(File.expand_path( File.dirname(__FILE__) / '..' / '..' ), path.to_s)
+  # end
+  # Fuck that, using a constant. LAWL.
+  Scope = File.expand_path( File.dirname(__FILE__) / '..' / '..' )
 end
 
 require 'git-blog/parsers'
